33e9a55a3a77cc105fb15828d8a594a5906424ff
Khuong Pham <dkhuong291@gmail.com>
2015-11-09 23:53:33 +0700

    read JSON (movie's titles)

---
+++ RottenTomatoes/MoviesViewController.swift
@@ -0,0 +1,56 @@
+//
+//  MoviesViewController.swift
+//  RottenTomatoes
+//
+//  Created by Khuong Pham on 11/9/15.
+//  Copyright © 2015 Fantageek. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    var movies: [NSDictionary]?
+
+    @IBOutlet var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let url = NSURL(string: "https://coderschool-movies.herokuapp.com/movies?api_key=xja087zcvxljadsflh214")!
+        let request = NSURLRequest(URL: url)
+
+        NSURLConnection.sendAsynchronousRequest(request,
+            queue: NSOperationQueue.mainQueue()) { (response, data, error) -> Void in
+
+                let json = try? NSJSONSerialization.JSONObjectWithData(data!, options: .MutableContainers) as? NSDictionary
+                if let json = json {
+                    self.movies = json!["movies"] as? [NSDictionary]
+                    self.tableView.reloadData()
+                }
+        }
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let movies = movies {
+            return movies.count
+        } else {
+            return 0
+        }
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as UITableViewCell
+        let movie = movies![indexPath.row]
+        cell.textLabel?.text = movie["title"] as? String
+        return cell
+    }
+
+}
